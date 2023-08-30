c1d0963c7a4208f72761b748b7cdfdd8a5425c62
Anudeep0110 <anudeepgude765@gmail.com>
2023-08-30 10:53:18 +0530

    Reset Password Route

--- server.js
+++ server.js
@@ -47,13 +47,64 @@ app.post('/forgotpassword',(req,res) => {
                 from : 'anitsquiz01@gmail.com',
                 to : res1[0].mailId,
                 subject : 'Password Recovery Mail',
-                text: `Dear Students,
-
-                This is a reminder that the is scheduled on  at The quiz will last for  minutes.
-                
-                Please be prepared to participate on time and make the most of this opportunity. Good luck!
-                
-                Best regards,`
+                html: `<!DOCTYPE html>
+                <html>
+                <head>
+                <style>
+                  body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f4f4f4;
+                    margin: 0;
+                    padding: 0;
+                  }
+                  .container {
+                    max-width: 600px;
+                    margin: 0 auto;
+                    padding: 20px;
+                    background-color: #ffffff;
+                    border-radius: 10px;
+                    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+                  }
+                  .header {
+                    text-align: center;
+                    margin-bottom: 20px;
+                  }
+                  .header img {
+                    max-width: 200px;
+                    height: auto;
+                  }
+                  .content {
+                    padding: 20px;
+                    border-top: 1px solid #e0e0e0;
+                  }
+                  .btn {
+                    display: inline-block;
+                    padding: 10px 20px;
+                    background-color: #007bff;
+                    color: #ffffff;
+                    text-decoration: none;
+                    border-radius: 5px;
+                    font-weight: bold;
+                  }
+                </style>
+                </head>
+                <body>
+                  <div class="container">
+                    <div class="header">
+                      <img src="https://onlinesystem.anits.edu.in/images/logo.png" alt="Logo">
+                      <h1>Password Reset</h1>
+                    </div>
+                    <div class="content">
+                      <p>Hello,</p>
+                      <p>We received a request to reset your password. To proceed, click the button below:</p>
+                      <p><a class="btn" href="http://localhost:3000/resetpassword">Reset Password</a></p>
+                      <p>If you didn't request a password reset, please ignore this email.</p>
+                      <p>Best regards,<br>Anits Edusphere</p>
+                    </div>
+                  </div>
+                </body>
+                </html>
+                `
             };
             transporter.sendMail(mailOptions, (error, info) => {
                 if (error) {
@@ -65,6 +116,15 @@ app.post('/forgotpassword',(req,res) => {
         else{
             res.status(404).json(false);
         }
+    })
+})
+
+app.post('/resetpassword',(req,res) => {
+    const uname = req.body.uname;
+    const pass = req.body.pwd;
+    Login.updateOne({username:uname},{$set:{password:pass}})
+    .then((response) => {
+        console.log(response);
     })
 })
 
